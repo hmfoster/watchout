91c7b022de13096cf77cbfcc427f057990081cf9
Hailey Foster <haileyhasmail@gmail.com>
2015-07-11 17:31:48 -0700

    Refactored, styled

--- watchout.js
+++ watchout.js
@@ -1,18 +1,54 @@
-// start slingin' some d3 here.
+//style
+//get rid of setTimeout--> write helper function with 
+//".each('end',function(){helper(d3.select(this))})"
+//call with 'enemies'
 
-//If distance<radius*2, we have a collision!
+//Initial Settings
+var initialSettings = {
+  boardHeight: window.innerHeight,
+  boardWidth: window.innerWidth,
+  numberEnemies: 5,
+  radius: 20,
+  score: 0,
+  highScore: 0,
+  collisions: 0,
+};
+
+var randomizerX = function(){
+  return Math.random()*initialSettings.boardWidth;
+};
+var randomizerY = function(){
+  return Math.random()*initialSettings.boardHeight;
+};
+
+/////////SCORE FUNCTION
+setInterval(function(){
+  initialSettings.score++;
+  d3.select('.current').text("Current Score " + initialSettings.score);
+  }, 100);
+
+/////COLLISION & DRAG FUNCTIONS
+
+
+
 var collision = function(p1){
   var x1 = this.attributes.cx.value;//get cx attr;
   var x2 = d3.selectAll(".user")[0][0].attributes.cx.value;
   var y1 = this.attributes.cy.value;
   var y2 = d3.selectAll(".user")[0][0].attributes.cy.value;
   var distance = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-  if (distance<40){
-    console.log("COLLISION!!!");
-  }
+  
+  if (distance<40){ 
+      initialSettings.collisions++;
+      d3.select('.collisions').text("Atomic Collisions "+initialSettings.collisions);
+    
+    if(initialSettings.score>initialSettings.highScore){
+      initialSettings.highScore = initialSettings.score;
+      d3.select('.high').text("High Score "+ initialSettings.highScore);
+    }
+    initialSettings.score = 0;
+  } 
 };
-
-var enemyData = [1,2,3,4,5];
 
 var drag = d3.behavior.drag().on(
           'drag', function(){
@@ -20,41 +56,48 @@ var drag = d3.behavior.drag().on(
                   .attr('cy', d3.event.y);
               });
 
+
+
+
 // Creating a conatiner
-var svgContainer = d3.select('body').append('svg').attr("width", 600).attr("height", 600);
+var svgContainer = d3.select('body')
+                    .append('svg')
+                    .attr("width", initialSettings.boardWidth)
+                    .attr("height", initialSettings.boardHeight);
 
-//Add enemies to svg container
-var enemies = svgContainer.selectAll('circle').data(enemyData).enter().append('circle');
 
-enemies.classed('enemy',true);
 
-var enemiesStyle = enemies.attr('cx', function(d,i){ return d.x;})
-                  .attr('cy', function(d, i){return d.y;}).attr('r', 20).style('fill', 'green');
+//Add enemies and user to svg container
+var enemies = svgContainer.selectAll('circle')  
+                          .data(d3.range(initialSettings.numberEnemies))
+                          .enter()
+                          .append('circle')
+                          .classed('enemy',true)
+                          .attr('r', initialSettings.radius)
+                          .attr('cx', randomizerX)
+                          .attr('cy', randomizerY)
+                          .style('fill', 'green');
  
-//Make a tween function
-setInterval(function(){ enemies.transition()
-                      .tween('.enemy',function(){
-                        return collision;
-                      })
-                      .ease('elastic')
-                      .duration(1000)
-                      .attr('cx', function(d,i){ 
-                        return 20+Math.random()*550;})
-                      .attr('cy', function(d,i){
-                        return 20+Math.random()*550;
-                      })
-                      .attr('r', 20);
-                      },1000);
+var user = svgContainer.selectAll('circle.a')
+                      .data([1])
+                      .enter()
+                      .append('circle')
+                      .classed('user',true)
+                      .attr('cx', initialSettings.boardWidth/2)
+                      .attr('cy', initialSettings.boardHeight/2)
+                      .attr('r', initialSettings.radius)
+                      .style('fill', 'red')
+                      .call(drag);
 
-var userData = [{x:300, y:300, r:20}];
+var move = function(){
+                  enemies.transition()
+                  .tween('.enemy',function(){return collision;})
+                  .ease('elastic')
+                  .duration(1000)
+                  .attr('cx', randomizerX)
+                  .attr('cy', randomizerY)
+                  .each('end', function(){move(d3.select(this));
+                  });  
+                };
 
-var user = svgContainer.selectAll('circle.a').data(userData).enter().append('circle');
-user.classed('user',true);
-var userStyle = user.attr('cx', function(d){ return d.x;})
-                  .attr('cy', function(d){return d.y;})
-                  .attr('r', function(d){ return d.r;})
-                  .style('fill', 'red')
-                  .call(drag);
- 
-//.tween('circle',function(d,i){
-                      // return collision(d);})
+ move(enemies);
